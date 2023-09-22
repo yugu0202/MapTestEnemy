2d873b80ee26503904ef0dd60ec8b82758d43ab5
yugu0202 <yugu0202@users.noreply.github.com>
2023-09-22 20:11:08 +0900

    とりあえず壁よけだけ

---
+++ look_every_round.rb
@@ -0,0 +1,52 @@
+# -*- coding: utf-8 -*-
+require 'CHaserConnect.rb' #呼び出すおまじない
+require 'CHaserTools.rb'
+
+include Action
+include Direction
+include MapInfo
+
+# 書き換えない
+target = CHaserConnect.new("prac") # ()の中好きな名前
+values = Array.new(10)
+random = Random.new # 乱数生成
+
+direction = Direction::UP
+
+def create_can_move values
+  can_move = []
+
+  2.step(9, 2) { |i|
+    if values[i] != MapInfo::BLOCK
+      can_move.push(i)
+    end
+  }
+
+  return can_move
+end
+
+#--------ここから--------
+loop do # ここからループ
+
+#---------ここから---------
+  values = target.getReady
+
+  if values[0] == 0
+    break
+  end
+#-----ここまで書き換えない-----
+
+can_move = create_can_move(values)
+
+direction = can_move.sample
+
+act_values = target.order(Action::WALK, direction)
+
+#---------ここから---------
+  if values[0] == 0
+    break
+  end
+
+end # ループここまで
+target.close
+#-----ここまで書き換えない-----
