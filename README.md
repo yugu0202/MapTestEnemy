4cb4b76e94e9cd4f54d7d4c28a4fb8018810b2a5
sinohiro <sinohiro966@gmail.com>
2023-09-25 19:17:04 +0900

    add get item

---
+++ README.md
@@ -0,0 +1,10 @@
+# Map Test Enemy
+CHaser釧路大会のマップ品質向上のためのサンプルキャラクター集
+
+## avoid_wall_onry
+* 壁に当たるまで同じ方向に動き続けるプログラム
+
+## look_every_round
+* 毎ターンlookして行動する
+* lookした結果からアイテムが取れる場合は回収する
+* lookした結果から敵がいた場合
--- avoid_wall_onry.rb
+++ avoid_wall_onry.rb
@@ -7,22 +7,43 @@ include Direction
 include MapInfo
 
 # 書き換えない
-target = CHaserConnect.new("avoid wall onry") # ()の中好きな名前
+target = CHaserConnect.new("avoid wall") # ()の中好きな名前
 values = Array.new(10)
 random = Random.new # 乱数生成
 
 direction = Direction::UP
 
-def create_can_move values
+def create_can_move values, direction
   can_move = []
 
-  2.step(9, 2) { |i|
-    if values[i] != MapInfo::BLOCK
-      can_move.push(i)
-    end
+  can_move = [2, 4, 6, 8].select { |i|
+    values[i] != MapInfo::BLOCK
   }
 
   return can_move
+end
+
+def select_move_to_wall can_move, direction
+  return direction if can_move.size.zero?
+
+  if can_move.include?(direction)
+    return direction
+  else
+    return can_move.sample(random)
+  end
+end
+
+def get_item values, move_direction
+  return move_direction if !values.include?(MapInfo::ITEM)
+
+  item = [2, 4, 6, 8].select { |i| values[i] == MapInfo::ITEM }
+
+  if !item.size.zero?
+    return item
+  else
+    return get_slash_item(values, move_direction)
+  end
+
 end
 
 #--------ここから--------
@@ -38,13 +59,13 @@ loop do # ここからループ
 
 can_move = create_can_move(values)
 
-if can_move.include?(direction)
-  act_values = target.order(Action::WALK, direction)
-else
-  # 同じ方向が含まれていない場合、ランダムに選択
-  direction = can_move.sample
-  act_values = target.order(Action::WALK, direction)
-end
+move_to_wall = select_move_to_wall(can_move, direction)
+
+move_direction = get_item(values, move_direction)
+
+direction = move_direction.sample
+
+act_values = target.order(Action::WALK, direction)
 
 #---------ここから---------
   if values[0] == 0
